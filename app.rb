05a11adf662cbeb7fa020378268eb56dbfeb0b75
Harley Rayner <harleymteden@gmail.com>
2018-11-22 06:10:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number]
     @phrase = params[:phrase]
-    "#{@phrase} "*@number.to_s
+    "#{@phrase*@number} "
   end
 
 
